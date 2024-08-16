937aee554e7cebc040ca3aa744595efad72cda7a
taiphanvan2k3 <taiphanvan2403@gmail.com>
2024-08-17 00:25:43 +0700

    feat: Apply Swagger for project

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { UsersService } from './users.service';
 
+@ApiTags('users')
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private readonly userService: UsersService) {}
+
+    @Get()
+    @ApiOkResponse({ description: 'Returns an array of users', type: [String] })
+    getUsers(): string[] {
+        return this.userService.getUsers();
+    }
+}
