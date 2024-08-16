114e63f56a8beb9b381fafd6c87b77fe5f387e2f
taiphanvan2k3 <taiphanvan2403@gmail.com>
2024-08-17 00:04:19 +0700

    feat: Init project

---
+++ src/app.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@nestjs/common';
+
+// Dùng annotation @Injectable() để đánh dấu class này có thể dùng để inject vào các class khác
+@Injectable()
+export class AppService {
+    getHello(): string {
+        return 'Hello World! 123';
+    }
+}
